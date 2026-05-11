72ce97f66bd9692edf2b77ff728887821315b720
hrishob <hrishobp@gmail.com>
2026-05-11 17:25:11 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app =express();
+const PORT = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+}) 
